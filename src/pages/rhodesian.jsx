5ec992b391e89733e7fd1b58f37da87f8ba4afa1
Alejandra Bastidas <bastidasaleja@hotmail.com>
2021-09-27 10:51:12 -0500

    bootstrap

--- src/pages/rhodesian.jsx
+++ src/pages/rhodesian.jsx
@@ -1,8 +1,14 @@
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 function RhodesianInfoPage() {
     return (
-        <div>Informacion Rhosesian</div>
+        <div>Informacion Rhodesian
+           <section>
+           <button type="button" class="btn btn-secondary">Secondary</button>
+           <button type="button" class="btn btn-primary">Primary</button>
+            </section> 
+            
+        </div>
 
     );
 }
